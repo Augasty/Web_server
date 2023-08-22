171e4062016281d24dbc77853012477b34d69a99
Augasty <sayaksengupta140@gmail.com>
2023-08-22 22:52:03 +0530

    updates

--- main.go
+++ main.go
@@ -6,17 +6,22 @@ import (
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) { //response and requests are the parameters
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	//parameters are response and requests are the parameters
+
+	// error handling
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
 
+	// if the method is incorrect
 	if r.Method != "GET" {
 		http.Error(w, "Method is not supported.", http.StatusNotFound)
 		return
 	}
 
+	// actual right scenario
 	fmt.Fprintf(w, "hello")
 }
 
@@ -25,6 +30,8 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
+
+	// ideal scenario
 	fmt.Fprintf(w, "POST request successful")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
@@ -34,10 +41,12 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	fileserver := http.FileServer(http.Dir("./static"))
+	// routes
 	http.Handle("/", fileserver)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
+	// on starting up the server
 	fmt.Printf("Starting a new server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
